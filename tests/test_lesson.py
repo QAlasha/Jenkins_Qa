0516850df83703b1cac849abbbbcb02b55ddd1ff
lashaQA <lasha.bas@mail.ru>
2024-11-02 19:37:08 +0300

    refactor223

--- tests/test_lesson.py
+++ tests/test_lesson.py
@@ -6,7 +6,7 @@ def test_one():
     URL = 'https://stage-web.pfm.team/'
     chrome_options = Options()
     chrome_options.add_argument("--headless")
-    driver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub', options=chrome_options)
+    driver = webdriver.Chrome()
     driver.implicitly_wait(time_to_wait=25)
     driver.get(URL)
     assert driver.current_url == URL, f'URL: {driver.current_url}'
